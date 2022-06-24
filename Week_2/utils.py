d59c4082467b5287ca783aa405bc6d8bfe1ea8dd
Andres Noboa V <56968012+aenoboa1@users.noreply.github.com>
2022-06-24 02:05:49 -0500

    utils

--- Week_2/utils.py
+++ Week_2/utils.py
@@ -1,4 +1,4 @@
-
+## Data Fetcher for green tripdata 
 ## Load data per year
 
 import os
@@ -6,26 +6,22 @@ import sys
 from pathlib import Path
 import numpy as np
 
-    
-#def __init__(self):
-#    self.month = sys.argv[0]
-#    self.year = sys.argv[1]
-
-def download_data(month,year):
+def download_data(month_start,month_end,year):
     dataset = "green_tripdata"
     # Define month range
-    myList = np.linspace(1,3,3) # From Jan to Feb
+    myList = np.linspace(int(month_start),int(month_end),int(month_end)) # From Jan to Feb
 
     Path(f'./data/{year}/').mkdir(parents=True,exist_ok=True)
     for a in myList:
-        print(f"-_-_-_-_-_- Fetching Data for {year}/{month} ")
+        print(f"-_-_-_-_-_- Fetching Data for {year}/{int(a)} ")
         
-        os.system(f"wget -O ./data/2021/{dataset}_{year}-0{a.astype(int)}.parquet \
+        os.system(f"wget -O ./data/{year}/{dataset}_{year}-0{a.astype(int)}.parquet \
             https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset}_{year}-0{a.astype(int)}.parquet")
-        
-    
+
+
 if __name__ == '__main__':
-    month = sys.argv[1]
-    year = sys.argv[2]
-    download_data(month, year)
+    year = sys.argv[1]
+    month_start = sys.argv[2]
+    month_end = sys.argv[3]
+    download_data(month_start,month_end, year)
         
