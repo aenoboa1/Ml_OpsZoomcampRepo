21a8bc1b71eb6495fbe724fbaeeb9093157a8c03
aenoboa1 <aenoboa1@espe.edu.ec>
2022-06-24 17:07:42 -0500

    ml_flow

--- Week_2/utils.py
+++ Week_2/utils.py
@@ -6,6 +6,7 @@ import sys
 from pathlib import Path
 import numpy as np
 
+
 def download_data(month_start,month_end,year):
     dataset = "green_tripdata"
     # Define month range
@@ -14,7 +15,6 @@ def download_data(month_start,month_end,year):
     Path(f'./data/{year}/').mkdir(parents=True,exist_ok=True)
     for a in myList:
         print(f"-_-_-_-_-_- Fetching Data for {year}/{int(a)} ")
-        
         os.system(f"wget -O ./data/{year}/{dataset}_{year}-0{a.astype(int)}.parquet \
             https://s3.amazonaws.com/nyc-tlc/trip+data/{dataset}_{year}-0{a.astype(int)}.parquet")
 
@@ -24,4 +24,3 @@ if __name__ == '__main__':
     month_start = sys.argv[2]
     month_end = sys.argv[3]
     download_data(month_start,month_end, year)
-        
